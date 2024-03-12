04c06f04192452b59cad0c976898e4c20bb6a151
clds <chintan.tops@gmail.com>
2024-03-12 17:58:19 +0530

    datatypes

---
+++ Learning_corejava/src/basic/Datatypes.java
@@ -0,0 +1,41 @@
+package basic;
+
+public class Datatypes {
+	public static void main(String[] args) {
+
+//		byte b = -128;
+//		short s = -32768;
+//		int i = 20000;
+//		long l = 565656565;
+//
+//		float f = 56.66656565f;
+//		double d = 5656.5656454564;
+//
+//		char ch = 'a';
+//
+//		boolean bool = false;
+
+//		System.out.println(f);
+//		System.out.println(d);
+		
+		//widening - implicite
+		int a = 10;  //4
+		long b = a;  //8
+		
+		//nerrowing - explicite
+		long i  = 145;
+		int j = (int) i;
+		
+
+//		System.out.println(b);
+//		System.out.println(j);
+		
+		
+		int k = 'd';
+		System.out.println(k);
+		
+		char c = 35;
+		System.out.println(c);
+		
+	}
+}
